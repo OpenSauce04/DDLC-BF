e0bff8bf350af58db604d539a712d2ab88f88e8e
OpenSauce04 <opensauce04@gmail.com>
2020-10-10 16:03:44 +0100

    Removed name box now has the dialogue box correctly positioned

--- functions/dialogue.py
+++ functions/dialogue.py
@@ -2,6 +2,7 @@ from functions.printline import printline
 from functions.printtext import printtext
 from functions.writeraw import writeraw
 from functions.clearline import clearline
+from functions.skipline import skipline
 def cleardialogue():
   for x in range(0,6):
     printtext(chr(27)+"[4"+str(3+x)+";0H")
@@ -13,6 +14,9 @@ def dialogue(speaking, text):
   if not (speaking==""):
     printline('+'+('-'*(len(speaking)+2))+'+')
     printline("| "+speaking+" |")
+  else:
+    skipline(2)
+
   printline('+'+('-'*148)+'+')
   printtext(text)
   writeraw(">,[-]<")
